89fbdadd78f19b80f613070f7fec818af3e20115
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-21 12:29:58 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,30 @@
 interface drinking{
-    fun tea()
+    fun tea(){print("Tea 1")}
     fun coffee()
 }
 
-class Tacher(val name: String, val surname: String){
+class Tacher(val name: String, val surname: String):drinking{
+    override fun tea() {
+        println("Pije podwójną herbatę")
+    }
+
+    override fun coffee() {
+        println("Pije małą kawę")
+    }
 
 }
 
-class Student(val name: String, val surname: String){
+class Student(val name: String, val surname: String):drinking{
+    override fun tea() {
+        println("Zamawia małą harbatę")
+    }
+
+    override fun coffee() {
+        println("Pije dużą kawę")
+    }
 
 }
 
 fun main(args: Array<String>) {
+
 }
