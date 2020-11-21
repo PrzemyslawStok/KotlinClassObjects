0d8442d5c40c57c490e570b037f9ec93465b5274
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-21 12:24:15 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,15 @@
+interface drinking{
+    fun tea()
+    fun coffee()
+}
+
+class Tacher(val name: String, val surname: String){
+
+}
+
+class Student(val name: String, val surname: String){
+
+}
+
 fun main(args: Array<String>) {
 }
